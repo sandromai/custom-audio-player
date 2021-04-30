fd21c7024f94b4bb715593e3e93b36d70c9847e0
Sandro Mai <sandromai6@gmail.com>
2021-04-29 23:12:50 -0300

    Initial Commit

---
+++ utils.js
@@ -0,0 +1,8 @@
+export const secondsToMinutes = time => {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+
+  return `${('0' + minutes).slice(-2)}:${('0' + seconds).slice(-2)}`;
+};
+
+export const path = file => `files/${file}`;
